7ef90a12104ad670d6cee9f17ea49e27b7b2c7b5
Serwaa <c.akotonimo@alustudent.com>
2025-07-14 13:48:33 +0200

    prediction

---
+++ Task 3/predict.py
@@ -0,0 +1,16 @@
+import requests
+import pandas as pd
+import numpy as np
+import joblib
+from datetime import datetime
+from tensorflow.keras.models import load_model
+import logging
+from typing import Optional, Dict, Any, List, Union
+from tensorflow.keras.losses import MeanSquaredError
+import time
+from datetime import datetime, timezone
+import random
+
+
+def predict(model, data):
+    return model.predict(data)
